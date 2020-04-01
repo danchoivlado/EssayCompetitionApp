f45eaf77f3dc3b182f5a6f8fd312acc73cd45cf4
dani dani <dani_yordani@abv.bg>
2020-04-01 13:44:43 +0300

    Created administation category page

---
+++ src/Web/EssayCompetition.Web/Areas/Administration/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿namespace EssayCompetition.Web.Areas.Administration.Controllers
+{
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CategoryController : AdministrationController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
