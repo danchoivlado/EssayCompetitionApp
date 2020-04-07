d28e6a1671a3da2a23c3afd4c6ab4a6d88f85938
Йордан Йорданов <dani_yordani@abv.bg>
2020-04-07 10:13:39 +0300

    Added teacher area

---
+++ src/Web/EssayCompetition.Web/Areas/Teacher/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿namespace EssayCompetition.Web.Areas.Teacher.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class DashboardController : TeacherController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ src/Web/EssayCompetition.Web/Areas/Teacher/Controllers/TeacherController.cs
@@ -0,0 +1,13 @@
+﻿namespace EssayCompetition.Web.Areas.Teacher.Controllers
+{
+    using EssayCompetition.Common;
+    using EssayCompetition.Web.Controllers;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = GlobalConstants.TeacherRoleName)]
+    [Area("Teacher")]
+    public class TeacherController : BaseController
+    {
+    }
+}
