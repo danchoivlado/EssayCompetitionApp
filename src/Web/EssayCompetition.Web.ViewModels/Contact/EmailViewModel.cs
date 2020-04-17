282c5267a42d0305dbc1ba1372d67f1201b9ab98
Йордан Йорданов <dani_yordani@abv.bg>
2020-04-17 21:12:29 +0300

    Aded contact form

---
+++ src/Web/EssayCompetition.Web.ViewModels/Contact/EmailViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace EssayCompetition.Web.ViewModels.Contact
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class EmailViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
--- src/Web/EssayCompetition.Web/Controllers/HomeController.cs
+++ src/Web/EssayCompetition.Web/Controllers/HomeController.cs
@@ -1,18 +1,28 @@
 ﻿namespace EssayCompetition.Web.Controllers
 {
     using System.Diagnostics;
+    using System.Threading.Tasks;
+    using EssayCompetition.Web.ViewModels.Contact;
 
     using EssayCompetition.Services.Data.CalendarServices;
     using EssayCompetition.Web.ViewModels;
     using Microsoft.AspNetCore.Mvc;
+    using EssayCompetition.Services.Messaging;
+    using SendGrid;
+    using SendGrid.Helpers.Mail;
+    using Microsoft.Extensions.Configuration;
 
     public class HomeController : BaseController
     {
         private readonly ICalendarService calendarService;
+        private readonly IEmailSender emailSender;
+        private readonly IConfiguration configuration;
 
-        public HomeController(ICalendarService calendarService)
+        public HomeController(ICalendarService calendarService, IEmailSender emailSender, IConfiguration configuration)
         {
             this.calendarService = calendarService;
+            this.emailSender = emailSender;
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
@@ -35,6 +45,23 @@
         public IActionResult About()
         {
             return this.View();
+        }
+
+        public IActionResult Contact()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmail(EmailViewModel viewModel)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(viewModel);
+            }
+
+            await this.emailSender.SendEmailAsync("essaycompetitionsender@abv.bg", viewModel.Email, "essaycompetitionsender@abv.bg", viewModel.Subject, viewModel.Message);
+            return this.View(viewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
