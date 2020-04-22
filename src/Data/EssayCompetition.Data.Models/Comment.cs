d0dc5dea1adc66fba23246fe77052144f0308887
Йордан Йорданов <dani_yordani@abv.bg>
2020-04-22 13:00:47 +0300

    Picked comment

---
+++ src/Data/EssayCompetition.Data.Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿namespace EssayCompetition.Data.Models
+{
+    using EssayCompetition.Data.Common.Models;
+
+    public class Comment : BaseDeletableModel<int>
+    {
+        public int EssayId { get; set; }
+
+        public virtual Essay Essay { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public virtual Comment Parent { get; set; }
+
+        public string Content { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
