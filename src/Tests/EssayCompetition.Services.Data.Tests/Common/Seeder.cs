470593bbe918190f44e814a4d7739d2dc82dead9
Йордан Йорданов <dani_yordani@abv.bg>
2020-04-27 16:29:56 +0300

    Aded tests for essayService

--- src/Tests/EssayCompetition.Services.Data.Tests/Common/Seeder.cs
+++ src/Tests/EssayCompetition.Services.Data.Tests/Common/Seeder.cs
@@ -1,6 +1,7 @@
 ﻿namespace EssayCompetition.Services.Data.Tests.Common
 {
     using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     using EssayCompetition.Data;
@@ -8,6 +9,8 @@
 
     public class Seeder
     {
+        private const string TestEmail = "test@abv.bg";
+
         public async Task SeedContestAsync(ApplicationDbContext context)
         {
             var category = new Category()
@@ -57,16 +60,30 @@
 
         public async Task<ApplicationUser> SeedUserAsync(ApplicationDbContext context, string userEmail)
         {
-            var user = new ApplicationUser() { Email = userEmail };
+            var user = new ApplicationUser() { Email = userEmail, UserName = userEmail};
 
             context.Users.Add(user);
             await context.SaveChangesAsync();
             return user;
         }
 
+        public async Task<IEnumerable<string>> SeedManyUserAsync(ApplicationDbContext context, int usersCount)
+        {
+            var userIds = new List<string>();
+            for (int i = 0; i < usersCount; i++)
+            {
+                var user = new ApplicationUser() { Email = TestEmail };
+                context.Users.Add(user);
+                userIds.Add(user.Id);
+            }
+
+            await context.SaveChangesAsync();
+            return userIds;
+        }
+
         public async Task<ContestantContest> SeedContestantContestAsync(ApplicationDbContext context)
         {
-            var user = await this.SeedUserAsync(context, "Test@abv.bg");
+            var user = await this.SeedUserAsync(context, TestEmail);
             var contest = await this.SeedFutureContestAsync(context);
 
             var contestantContest = new ContestantContest()
@@ -78,6 +95,63 @@
             context.ContestantContest.Add(contestantContest);
             await context.SaveChangesAsync();
             return contestantContest;
+        }
+
+        public async Task<Essay> SeedEssayAsync(ApplicationDbContext context)
+        {
+            var user = await this.SeedUserAsync(context, TestEmail);
+            var contest = await this.SeedFutureContestAsync(context);
+
+            Essay essay = new Essay()
+            {
+                ContestId = contest.Id,
+                UserId = user.Id,
+            };
+
+            context.Essays.Add(essay);
+            await context.SaveChangesAsync();
+            return essay;
+        }
+
+        public async Task<Essay> SeedEssayAsync(ApplicationDbContext context, string userId, int contestId)
+        {
+            Essay essay = new Essay()
+            {
+                ContestId = contestId,
+                UserId = userId,
+                Graded = true,
+            };
+
+            context.Essays.Add(essay);
+            await context.SaveChangesAsync();
+            return essay;
+        }
+
+        public async Task SeedManyEssaysAsync(ApplicationDbContext context, int essaysCount)
+        {
+            var userIds = await this.SeedManyUserAsync(context, essaysCount);
+            var contest = await this.SeedFutureContestAsync(context);
+
+            foreach (var userId in userIds)
+            {
+                await this.SeedEssayAsync(context, userId, contest.Id);
+            }
+
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<string> SeedUserManyEssaysAsync(ApplicationDbContext context, int essaysCount)
+        {
+            var user = await this.SeedUserAsync(context, TestEmail);
+            var contest = await this.SeedFutureContestAsync(context);
+
+            for (int i = 0; i < essaysCount; i++)
+            {
+                await this.SeedEssayAsync(context, user.Id, contest.Id);
+            }
+
+            await context.SaveChangesAsync();
+            return user.Id;
         }
     }
 }
---
+++ src/Tests/EssayCompetition.Services.Data.Tests/EssayServiceTests.cs
@@ -0,0 +1,160 @@
+﻿using EssayCompetition.Data.Models;
+using EssayCompetition.Data.Repositories;
+using EssayCompetition.Services.Data.EssayServices;
+using EssayCompetition.Services.Data.Tests.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using EssayCompetition.Web.ViewModels.Essays;
+using EssayCompetition.Services.Mapping;
+using System.Reflection;
+
+namespace EssayCompetition.Services.Data.Tests
+{
+    public class EssayServiceTests
+    {
+        private const int CurrentPage = 1;
+        private const int PageSize = 2;
+        private Seeder seeder;
+
+        public EssayServiceTests()
+        {
+            this.seeder = new Seeder();
+            AutoMapperConfig.RegisterMappings(
+                typeof(Essay).GetTypeInfo().Assembly,
+                typeof(EssayViewModel).GetTypeInfo().Assembly);
+        }
+
+        [Fact]
+        public async Task GetEssayDetailsTest()
+        {
+            var context = EssayCompetitionContextInMemoryFactory.InitializeContext();
+            var essay = await this.seeder.SeedEssayAsync(context);
+            var essayRepository = new EfDeletableEntityRepository<Essay>(context);
+            var service = new EssayService(essayRepository);
+
+            var resultedEssay = service.GetEssayDetails<EssayViewModel>(essay.Id);
+
+            Assert.True(essay.UserId == resultedEssay.UserId, "GetEssayDetails method does not work correctly");
+        }
+
+        [Fact]
+        public async Task GetEssaysInRangeTest()
+        {
+            var context = EssayCompetitionContextInMemoryFactory.InitializeContext();
+            await this.seeder.SeedManyEssaysAsync(context, 4);
+            var essayRepository = new EfDeletableEntityRepository<Essay>(context);
+            var service = new EssayService(essayRepository);
+
+            var resultedEssays = service.GetEssaysInRange<EssayViewModel>(CurrentPage, PageSize);
+
+            Assert.True(resultedEssays.Count() == (context.Essays.Count() - PageSize), "GetEssaysInRange method does not work correctly");
+        }
+
+        [Fact]
+        public async Task GetEssaysFromUserWithIdInRangeTest()
+        {
+            var context = EssayCompetitionContextInMemoryFactory.InitializeContext();
+            var userId = await this.seeder.SeedUserManyEssaysAsync(context, 4);
+            var essayRepository = new EfDeletableEntityRepository<Essay>(context);
+            var service = new EssayService(essayRepository);
+
+            var resultedEssays = service.GetEssaysFromUserWithIdInRange<EssayViewModel>(userId, CurrentPage, PageSize);
+
+            Assert.True(resultedEssays.Count() == (context.Essays.Count() - PageSize), "GetEssaysFromUserWithIdInRange method does not work correctly");
+        }
+
+        [Fact]
+        public async Task GetUserEssaysCountTest()
+        {
+            var context = EssayCompetitionContextInMemoryFactory.InitializeContext();
+            var expectedCount = 4;
+            var userId = await this.seeder.SeedUserManyEssaysAsync(context, expectedCount);
+            var essayRepository = new EfDeletableEntityRepository<Essay>(context);
+            var service = new EssayService(essayRepository);
+
+            var resultedCount = service.GetUserEssaysCount(userId);
+
+            Assert.True(resultedCount == expectedCount, "GetUserEssaysCount method does not work correctly");
+        }
+
+        [Fact]
+        public async Task GetEssayCountTest()
+        {
+            var context = EssayCompetitionContextInMemoryFactory.InitializeContext();
+            var expectedCount = 4;
+            var userId = await this.seeder.SeedUserManyEssaysAsync(context, expectedCount);
+            var essayRepository = new EfDeletableEntityRepository<Essay>(context);
+            var service = new EssayService(essayRepository);
+
+            var resultedCount = service.GetEssaysCount();
+
+            Assert.True(resultedCount == expectedCount, "GetEssayCount method does not work correctly");
+        }
+
+        [Fact]
+        public async Task GetEssayNameTest()
+        {
+            var context = EssayCompetitionContextInMemoryFactory.InitializeContext();
+            var seededEssay = await this.seeder.SeedEssayAsync(context);
+            var essayRepository = new EfDeletableEntityRepository<Essay>(context);
+            var service = new EssayService(essayRepository);
+
+            var resultedName = service.GetEssayName(seededEssay.UserId, seededEssay.ContestId);
+
+            Assert.True(resultedName == seededEssay.Title, "GetEssayName method does not work correctly");
+        }
+
+        [Fact]
+        public async Task GetEssayIdTest()
+        {
+            var context = EssayCompetitionContextInMemoryFactory.InitializeContext();
+            var seededEssay = await this.seeder.SeedEssayAsync(context);
+            var essayRepository = new EfDeletableEntityRepository<Essay>(context);
+            var service = new EssayService(essayRepository);
+
+            var resultedId = service.GetEssaysId(seededEssay.UserId, seededEssay.ContestId);
+
+            Assert.True(resultedId == seededEssay.Id, "GetEssayId method does not work correctly");
+        }
+
+        [Fact]
+        public async Task HasEssayWithIdTest()
+        {
+            var context = EssayCompetitionContextInMemoryFactory.InitializeContext();
+            var seededEssay = await this.seeder.SeedEssayAsync(context);
+            var essayRepository = new EfDeletableEntityRepository<Essay>(context);
+            var service = new EssayService(essayRepository);
+
+            var result = service.HasEssayWithId(seededEssay.Id);
+
+            Assert.True(result == true, "HasEssayWithId method does not work correctly");
+        }
+
+        [Fact]
+        public async Task HasUserEssayTest()
+        {
+            var context = EssayCompetitionContextInMemoryFactory.InitializeContext();
+            var seededEssay = await this.seeder.SeedEssayAsync(context);
+            var essayRepository = new EfDeletableEntityRepository<Essay>(context);
+            var service = new EssayService(essayRepository);
+
+            var result = service.HasUserEssay(seededEssay.UserId, seededEssay.ContestId);
+
+            Assert.True(result == true, "HasUserEssay method does not work correctly");
+        }
+
+        [Fact]
+        public async Task HasAnyGradedEssayTest()
+        {
+            var context = EssayCompetitionContextInMemoryFactory.InitializeContext();
+            var seededEssay = await this.seeder.SeedEssayAsync(context);
+            var essayRepository = new EfDeletableEntityRepository<Essay>(context);
+            var service = new EssayService(essayRepository);
+
+            var result = service.HasAnyGradedEssay();
+
+            Assert.True(result == false, "HasAnyGradedEssay method does not work correctly");
+        }
+    }
+}
